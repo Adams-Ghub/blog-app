ae7b519552561317eb63ae27bdae67416348a9b0
Adams-Ghub <bamfoadamsf@gmail.com>
2023-07-20 17:31:58 +0000

    Create migration file for posts table

---
+++ db/migrate/20230720125622_create_posts.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Creates the posts table in the database
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.integer :LikesCounter
+      t.string :CommentsCounter
+
+      t.timestamps
+    end
+  end
+end
