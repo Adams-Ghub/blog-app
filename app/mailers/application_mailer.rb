8d1a0c136938379a8bf1d6432cd85718b07b4640
Adams-Ghub <bamfoadamsf@gmail.com>
2023-07-20 17:40:18 +0000

    Correct linter errors

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# Helps manage the mailing system of the app
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# The Application record class
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
