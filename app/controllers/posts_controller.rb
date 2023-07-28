de45613639f372033d6ad5599d0706a9696f684c
Adams-Ghub <bamfoadamsf@gmail.com>
2023-07-28 20:53:03 +0000

    Modify post controller to allow commenting and liking of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,34 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
-    # puts @posts
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+    @user = User.find(params[:user_id])
+    @current_user = current_user
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    @post.author_id = current_user.id
+
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post created successfully.'
+    else
+      render :new
+    end
   end
 end
