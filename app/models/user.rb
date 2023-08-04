3fbae41bc09bfaf6ffbbaf388f1d32d91d74c37a
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-04 13:01:59 +0000

    Style the login and sign up pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :likes, foreign_key: 'users_id'
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'users_id'
 
   validates :name, presence: true
-  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  #validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def show_most_recent_post
     Post.where(author_id: id).order(created_at: :desc).limit(3)
