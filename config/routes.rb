5fe63367a46a4bf511152d44ca5aa2d30fc8842a
massa <zoumaniguimassa12@gmail.com>
2023-08-05 17:06:41 +0000

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do 
   devise_for :users
-  root 'users#index'
-  resources :users, only: [:index, :show,] do
-    resources :posts,  only: [:index, :create, :new, :show, :destroy] do
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:create]
+  root 'users#index' 
+  namespace :api do
+    resources :users, only: [] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
     end
   end
-end
+
+    resources :users, only: [:index, :show,] do
+      resources :posts,  only: [:index, :create, :new, :show, :destroy] do
+        resources :comments, only: [:new, :create, :destroy]
+        resources :likes, only: [:create]
+      end
+    end
+  end
