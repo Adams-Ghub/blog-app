e51d060aa01b5a683320b22430e789bf7fba2c3d
massa <zoumaniguimassa12@gmail.com>
2023-08-05 14:54:22 +0000

    Implementing comments_controller.rb file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,4 +23,18 @@ class CommentsController < ApplicationController
       render 'posts/show'
     end
   end
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    authorize! :destroy, @comment
+   
+    @comment.update_comment_counter
+    if @comment.destroy
+      @comment.update_comment_counter 
+      redirect_to user_post_path(@user, @post), notice: 'Comment deleted successfully.'
+    else
+      redirect_to user_post_path(@user, @post), notice: 'Failed to delete comment.'
+    end
+  end
 end
