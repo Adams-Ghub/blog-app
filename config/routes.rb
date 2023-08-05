a54aff58e4293abc367b2896efdd3ddb87d7985c
massa <zoumaniguimassa12@gmail.com>
2023-08-05 18:51:24 +0000

    Run rails generate rspec:swagger API::CommentsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do 
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
   root 'users#index' 
   namespace :api do
